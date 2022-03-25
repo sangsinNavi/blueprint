98e0eb4993678f38c4bc3862d3578cdd65e95923
SangSin <sangsin.lee@naviworks.com>
2022-03-25 15:05:28 +0900

    second

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import {NavbarExample} from './components/NavbarExample'
 import ToggleContainer from './components/ToggleContainer';
-import { PanelStack2Example } from './components/panelStackExample';
+import { TabsExample } from './components/tabExample';
 
 function App(): JSX.Element {
   return (
     <div id="body">
-      {/* <NavbarExample id="a"/>
-      <ToggleContainer /> */}
-      <PanelStack2Example id="a" />
+      <NavbarExample id="a"/>
+      <ToggleContainer />
+      <TabsExample id="a" />
     </div>
   );
 }
